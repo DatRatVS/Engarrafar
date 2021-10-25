ee7db72786b9a34a6ea34f14dbe1385d0578c484
Rian <49768896+DatRatVS@users.noreply.github.com>
2021-10-25 16:56:31 -0300

    1.1 - Now using some fancy Bukkit Tasks to handle interactions with player inventories. Thanks Focamacho <3.

--- src/main/java/datrat/engarrafar/commands/Engarrafar.java
+++ src/main/java/datrat/engarrafar/commands/Engarrafar.java
@@ -1,16 +1,17 @@
 package datrat.engarrafar.commands;
 
+import com.google.common.collect.Lists;
 import datrat.engarrafar.utility.Experience;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.List;
+
+import static datrat.engarrafar.EngarrafarMain.instance;
 
 public class Engarrafar implements CommandExecutor {
 
@@ -28,43 +29,34 @@ public class Engarrafar implements CommandExecutor {
 
 			int bottlesToGive = playerExp / 8;
 
+			player.setExp(0);
+			player.setLevel(0);
+
 			if (bottlesToGive == 0) {
 				player.sendMessage(ChatColor.RED + "" + ChatColor.ITALIC + "Você não tem experiência o suficiente para engarrafar!");
 				return true;
 			}
 
-			if (player.getName().equals("PowerAz_G") && bottlesToGive == 1) {
-				player.sendMessage(ChatColor.RED + "" + ChatColor.ITALIC + "iala jk, tu engarrafô " + bottlesToGive + " bottle!");
-			} else if (player.getName().equals("PowerAz_G") && bottlesToGive > 1) {
-				player.sendMessage(ChatColor.RED + "" + ChatColor.ITALIC + "iala jk, tu engarrafô " + bottlesToGive + " bottles!");
-			} else if (bottlesToGive == 1) {
+			if (bottlesToGive == 1) {
 				player.sendMessage(ChatColor.RED + "" + ChatColor.ITALIC + "Você engarrafou " + bottlesToGive + " bottle!");
 			} else {
 				player.sendMessage(ChatColor.RED + "" + ChatColor.ITALIC + "Você engarrafou " + bottlesToGive + " bottles!");
 			}
 
-			player.setExp(0);
-			player.setLevel(0);
-
-			Location loc = player.getLocation();
-			World world = player.getWorld();
-
-			if (player.getInventory().firstEmpty() == -1) {
-				player.sendMessage(ChatColor.RED + "" + ChatColor.ITALIC + "Seu inventario está ou quase está cheio, dropando garrafas no chão.");
-				for (int i = bottlesToGive; i > 0; i--) {
-					world.dropItemNaturally(loc, ExperienceBottle());
-				}
-			} else {
-				for (int i = bottlesToGive; i > 0; i--) {
-					player.getInventory().addItem(ExperienceBottle());
-				}
+			List<ItemStack> bottles = Lists.newArrayList();
+			while(bottlesToGive > 0) {
+				int toGive = Math.min(bottlesToGive, 64);
+				bottles.add(new ItemStack(Material.EXPERIENCE_BOTTLE, toGive));
+				bottlesToGive -= toGive;
 			}
+			Bukkit.getScheduler().callSyncMethod(instance, () -> {
+				for (ItemStack item : bottles) {
+					player.getInventory().addItem(item).values().forEach(rejected -> player.getWorld().dropItem(player.getLocation(), rejected));
+				}
+				return true;
+			});
 			return true;
 		}
 		return false;
-	}
-
-	public ItemStack ExperienceBottle() {
-		return new ItemStack(Material.EXPERIENCE_BOTTLE);
 	}
 }
